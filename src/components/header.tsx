6c391782757b7dccdf47e743fc8ce8922474712c
Ivan <imikon@proton.me>
2024-11-18 16:40:58 +0100

    styled header buttons with active states, added clsx and framer motion into deps

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,11 @@
+'use client'
+
 import React from 'react';
 import Link from 'next/link';
 import Logo from '@/components/logo';
+import {usePathname} from 'next/navigation';
+import clsx from 'clsx';
+import {motion} from 'framer-motion';
 
 const routes = [
 	{
@@ -9,23 +14,35 @@ const routes = [
 	},
 	{
 		name: 'All events',
-		path: 'events/all'
+		path: '/events/all'
 	}
 ]
 
 const Header = () => {
+	const activePath = usePathname()
+	
+	console.log(activePath)
+	
 	return (
 		<header className={'flex items-center justify-between w-full border-b border-white/10 h-14 px-3 md:px-9'}>
 			<Logo/>
 			
-			<nav>
-				<ul className={'flex gap-2'}>
-					{routes.map(route => (
-						<li
-							key={route.path}
-							className={'text-white/50 hover:text-white active:text-white transition'}
-						><Link href={route.path}>{route.name}</Link></li>
-					))}
+			<nav className={'h-full'}>
+				<ul className={'flex h-full gap-6'}>
+					{routes.map(route => {
+						return (
+							<li
+								key={route.path}
+								className={clsx('flex items-center hover:text-white transition relative', {
+									'text-white': activePath === route.path,
+									'text-white/50': activePath !== route.path
+								})}
+							><Link href={route.path}>{route.name}</Link>
+								{activePath === route.path &&
+                                    <motion.div layoutId={'nav-buttons'} className={'bg-accent h-1 w-full absolute bottom-0'}></motion.div>}
+							</li>
+						)
+					})}
 				</ul>
 			</nav>
 		</header>
